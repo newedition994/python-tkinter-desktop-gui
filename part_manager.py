706fb951a4285cb1f46ed75bcb2ceeaa7410aee3
Breyanna Vass <breyannavass@gmail.com>
2019-11-10 18:13:23 -0500

    labeling parts of the application

--- part_manager.py
+++ part_manager.py
@@ -3,6 +3,11 @@ from tkinter import *
 # Create window object
 app = Tk()
 
+# part
+part_text = StringVar()
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
+part_label.grid(row=0, column=0)
+
 app.title('Part Manager')
 app.geometry('700x350')
 
