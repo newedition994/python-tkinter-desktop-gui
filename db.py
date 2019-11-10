c0c9087eb45ec0e157d374aea35db3fd283b3c2b
Breyanna Vass <breyannavass@gmail.com>
2019-11-10 18:58:58 -0500

    making a database class

---
+++ db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+class Database:
+    def __init__(self, db):
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS parts (id INTEGER PRIMARY KEY, part text, customer text, retailer text, price text)"
+        )
+        self.conn.commit()
