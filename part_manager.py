f44ec536e3917c43cef3746e56b0485e082f49cd
Breyanna Vass <breyannavass@gmail.com>
2019-11-10 18:22:13 -0500

    adding customer and retailer

--- part_manager.py
+++ part_manager.py
@@ -3,10 +3,33 @@ from tkinter import *
 # Create window object
 app = Tk()
 
-# part
+# part1
 part_text = StringVar()
 part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
-part_label.grid(row=0, column=0)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
+
+# Customer
+customer_text = StringVar()
+customer_label = Label(app, text='Customer Name', font=('bold', 14))
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry = Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+
+# Retailer
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer', font=('bold', 14))
+retailer_label.grid(row=0, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=0, column=1)
+
+# part 4
+part_text = StringVar()
+part_label = Label(app, text='Part Name', font=('bold', 14))
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
 
 app.title('Part Manager')
 app.geometry('700x350')
